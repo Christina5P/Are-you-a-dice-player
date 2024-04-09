67b8baf5e4aa130c3488487174713262a43a2218
Christina_5P Åhman <christina.ahman@tv4.se>
2024-04-09 22:20:30 +0000

    try to show dots instead of number on dice

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,36 @@ const myPlayBtn = document.getElementById("play")
       constbuttons = document.getElementsByClassName("control");
       const playerScore = document.getElementById("player-score");
       const dice = document.getElementById("dice");
-      const diceValue = document.getElementById("dice-value")
-      const computerImage = document.getElementById("computer");
-      const messages = document.getElementById("message");
+      const diceValue = document.getElementById("dice-value");
       const choices = ["1", "2", "3", "4", "5", "6"];
+    
 
       dice.addEventListener("click", rollDice);
 // roll Dice and get computers number
       function rollDice() {
-        var randomNumber = Math.floor(Math.random() * 6) + 1;
+        const numberOfRolls = dice.querySelectorAll("span").length;
+        const randomNumber = Math.floor(Math.random() * numberofRolls) + 1;
         diceValue.textContent = randomNumber;
-      }
+               
+        message.textContent = "You rolled: " + randomNumber;
 
+        // clear dice from used number
+        dice.innerHTML = "";
+
+        for (let i = 0; i < randomNumber; i++) {
+          const dot = document.createElement("span");
+          dot.classList.add("roll-dot");
+          dice.appendChild(dot);
+        }
+      
+
+         setTimeout(function() {
+          diceValue.textContent = randomNumber;
+          dice.style.transition = 'none'; 
+          dice.style.transform = 'rotateY(0deg)';
+        }, 500); 
+      }
+    
+    
+    
      
