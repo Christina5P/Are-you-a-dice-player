2a79a28b71149c249831805eb5917c6553cdb486
Christina_5P Åhman <christina.ahman@tv4.se>
2024-04-10 17:42:54 +0000

    function roll dots workd but you dont see dots on dice yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,52 @@
-const myPlayBtn = document.getElementById("play")
+const myPlayBtn = document.getElementById("message");
+const playElement = document.getElementById("play");
  
- myPlayBtn.addEventListener('click', function(rolldice) {
-    console.log("StartNewGame");
-      });
-
          // This is variables
-      constbuttons = document.getElementsByClassName("control");
+      const buttons = document.getElementsByClassName("control");
       const playerScore = document.getElementById("player-score");
       const dice = document.getElementById("dice");
       const diceValue = document.getElementById("dice-value");
       const choices = ["1", "2", "3", "4", "5", "6"];
-    
-
-   //   dice.addEventListener("click", rollDice);
-
+     const numberOfRolls = 6
+             
 // roll Dice and get computers number
-      function rolldice() {
+      function rollDice() {
         const numberOfRolls = dice.querySelectorAll("span").length;
         const randomNumber = Math.floor(Math.random() * numberOfRolls) + 1;
-       // update dice with the number
-        diceValue.textContent = randomNumber;
-               
+      console.log ("dice number");
+
+     /*change random number to dots
+        for (let i = 0; i < randomNumber; i++) {
+        const dot = document.createElement("span");
+          dot.classList.add("roll");
+        dice.appendChild(dot);//to show dot in dice through DOM         
+        }*/
+
+        // update dice with the number                 
+      // dice.textContent = dicepatterns(randomNumber);
+       console.log("convert numbers to dots");
+       
+       message.textContent = "dice rolled: ";
+      }
+      
+      
    console.log("roll dice");
 
-     //change random number to dots
-        for (let i = 0; i < randomNumber; i++) {
-          const dot = document.createElement("span");
-          dot.classList.add("roll");
-          dice.appendChild(dot);//to show dot in dice through DOM
-        }
-        console.log("convert numbers to dots");
-      
-   // clear dice from used number
-   dice.innerHTML = "";
-
-      }
    /* tror inte jag behöver denna
          setTimeout(function() {
           diceValue.textContent = randomNumber;
           dice.style.transition = 'none'; 
           dice.style.transform = 'rotateY(0deg)';
         }, 500); 
-      } */
+      } 
     
+      const dicePatterns = [
+        "",
+         "*",
+         "* *",
+         "*   *",
+         "* * *",
+         "* *   *"
+      ];  
+      //convert numbers to dots  
+      dice.textContent = dicePatterns [randomNumber]; */
