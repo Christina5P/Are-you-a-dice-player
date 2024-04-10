6df9123ded802d479ca83d92f0ca29387e57969d
Christina_5P Åhman <christina.ahman@tv4.se>
2024-04-10 13:51:11 +0000

    notes in sheets

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Are-you-a-dice-player
+
+
+https://www.canva.com/
+Pictures when you win or loose
+
+https://betterprogramming.pub/creating-dice-in-flexbox-in-css-a02a5d85e516
+Tutoring to create dice with dots with css
+
+ChatGPT- 
+To get codes explained in Swedish
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 const myPlayBtn = document.getElementById("play")
  
- myPlayBtn.addEventListener('click', function() {
+ myPlayBtn.addEventListener('click', function(rolldice) {
     console.log("StartNewGame");
       });
 
+         // This is variables
       constbuttons = document.getElementsByClassName("control");
       const playerScore = document.getElementById("player-score");
       const dice = document.getElementById("dice");
@@ -11,32 +12,34 @@ const myPlayBtn = document.getElementById("play")
       const choices = ["1", "2", "3", "4", "5", "6"];
     
 
-      dice.addEventListener("click", rollDice);
+   //   dice.addEventListener("click", rollDice);
+
 // roll Dice and get computers number
-      function rollDice() {
+      function rolldice() {
         const numberOfRolls = dice.querySelectorAll("span").length;
-        const randomNumber = Math.floor(Math.random() * numberofRolls) + 1;
+        const randomNumber = Math.floor(Math.random() * numberOfRolls) + 1;
+       // update dice with the number
         diceValue.textContent = randomNumber;
                
-        message.textContent = "You rolled: " + randomNumber;
+   console.log("roll dice");
 
-        // clear dice from used number
-        dice.innerHTML = "";
-
+     //change random number to dots
         for (let i = 0; i < randomNumber; i++) {
           const dot = document.createElement("span");
-          dot.classList.add("roll-dot");
-          dice.appendChild(dot);
+          dot.classList.add("roll");
+          dice.appendChild(dot);//to show dot in dice through DOM
         }
+        console.log("convert numbers to dots");
       
+   // clear dice from used number
+   dice.innerHTML = "";
 
+      }
+   /* tror inte jag behöver denna
          setTimeout(function() {
           diceValue.textContent = randomNumber;
           dice.style.transition = 'none'; 
           dice.style.transform = 'rotateY(0deg)';
         }, 500); 
-      }
+      } */
     
-    
-    
-     
