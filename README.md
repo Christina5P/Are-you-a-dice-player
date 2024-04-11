e9d919849fe9ccd4a7e300bcff738489ed086d3d
Christina_5P Åhman <christina.ahman@tv4.se>
2024-04-11 11:30:43 +0000

    still struggling with diceroll and dots

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # Are-you-a-dice-player
-https://balsamiq.com/ - Build the skeleton
 
-https://www.canva.com/
+-https://balsamiq.com/ - Build the skeleton
+
+-https://www.canva.com/
 Pictures when you win or loose
 
-https://betterprogramming.pub/creating-dice-in-flexbox-in-css-a02a5d85e516
+-https://betterprogramming.pub/creating-dice-in-flexbox-in-css-a02a5d85e516
 Tutoring/inspiration to create dice with dots with css
 
-ChatGPT- 
+-https://www.youtube.com/watch?v=Fw3jE0tWn28 . Tutoriol of Change colours on buttons
+-ChatGPT- 
 To get codes explained in Swedish
 
-https://dcode.domenade.com/tutorials/how-to-create-a-dice-roll-game-with-html-css-and-javascript -tutoring dice and dots
+- https://dcode.domenade.com/tutorials/how-to-create-a-dice-roll-game-with-html-css-and-javascript -tutoring dice and dots
 
-https://github.com/bradtraversy/btre_project/issues/9 - troubleshot url-problems
+- https://www.kiltedviking.net/se/www/javascript/dom1.html - repetition(tutoring) of DOM
 
-https://favicon.io/ - For favicon to head
+- https://github.com/bradtraversy/btre_project/issues/9 - troubleshot url-problems
+
+- https://favicon.io/ - For favicon to head
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,22 @@ myPlayBtn.addEventListener('click', function() {
 const playElement = document.getElementById("message");
  
          // This is variables
-      const buttons = document.getElementsByClassName("control");
+     
+     
       const playerScore = document.getElementById("player-score");
-      const dice = document.querySelector("dice-dot");
-      //const dicedot = document.getElementsByclassName("dicedot");
       const choice = ["1", "2", "3", "4", "5", "6"]; // players choice of number
-     // const numberOfRolls = 6
-             
+          
+      const buttons = document.querySelectorAll(".control");
+      const board = document.getElementById("board");
+      
+      function changeBoardColor() {
+          board.style.backgroundColor = "white";
+          board.style.transition = ".5s ease";
+      }
+      
+      buttons.forEach(button => {
+          button.addEventListener("click", changeBoardColor);
+      });
          function playerchoice() { 
         choice= [i];
         for (i=0;i<els.length;i++) {
@@ -20,77 +29,97 @@ const playElement = document.getElementById("message");
         }
         }
 
-     function createDice(number){
-      const dotPositionMatrix = {
-        1: [
-          [50, 50]
-        ],
-        2: [
-          [20, 20],
-          [80, 80]
-        ],
-        3: [
-          [20, 20],
-          [50, 50],
-          [80, 80]
-        ],
-        4: [
-          [20, 20],
-          [20, 80],
-          [80, 20],
-          [80, 80]
-        ],
-        5: [
-          [20, 20],
-          [20, 80],
-          [50, 50],
-          [80, 20],
-          [80, 80]
-        ],
-        6: [
-          [20, 20],
-          [20, 80],
-          [50, 20],
-          [50, 80],
-          [80, 20],
-          [80, 80]
-        ]
-      };
+     
     
-      const dice = document.createElement("div");
+      
+
+     //create the dice
+    function createDice(number){     
+    const dotPositionMatrix = {
+      1: [
+        [50, 50]
+      ],
+      2: [
+        [20, 20],
+        [80, 80]
+      ],
+      3: [
+        [20, 20],
+        [50, 50],
+        [80, 80]
+      ],
+      4: [
+        [20, 20],
+        [20, 80],
+        [80, 20],
+        [80, 80]
+      ],
+      5: [
+        [20, 20],
+        [20, 80],
+        [50, 50],
+        [80, 20],
+        [80, 80]
+      ],
+      6: [
+        [20, 20],
+        [20, 80],
+        [50, 20],
+        [50, 80],
+        [80, 20],
+        [80, 80]
+      ]
+    };
+    const dice = document.createElement("div");
       dice.classList.add("dice");
 
       for (const dotPosition of dotPositionMatrix[number]) {
-        const dot = document.createElement("div");
+        const dot = document.createElement("span");
 
-        dot.classList.add("dicedot");
+        dot.classList.add("dice-container");
         dot.style.setProperty("--top", dotPosition[0] + "%");
         dot.style.setProperty("--left", dotPosition[1] + "%");
         dice.appendChild(dot);
       }
       return dice;
     }
+     // roll Dice with dots from dice-dot
 
-
-     // roll Dice and get computers number
      function rollDice() {
-      const diceContainer = document.querySelector('.dice-dot');
+      const diceContainer = document.getElementById('dice-container');
       const diceSpans = diceContainer.querySelectorAll('span');
+  
+      // Lägg till klassen "roll" för att starta animationssekvensen
+      diceSpans.forEach(span => span.classList.add("roll"));
+  
+      // Efter en viss tid, ta bort klassen "roll" för att avsluta animationssekvensen
+      setTimeout(() => {
+          diceSpans.forEach(span => span.classList.remove("roll"));
+      }, 1000);
+      message.textContent = "dice rolled: ";
+  }
+
+  /*  
+     
       const numberOfRolls = diceSpans.length;
       const randomNumber = Math.floor(Math.random() * numberOfRolls) + 1;
 
      //change random number to dots
      for (let i = 0; i < randomNumber; i++) {
       diceSpans[i].classList.add('dice');
-
-   
-     }
+  
+     } 
+     /* This is an alternative to spans
+      const diceContainer = document.getElementById("dice-container");
+     diceContainer.innerHTML = ""; // rensa innehåller
+     const dice = createDice(6);
+      diceContainer.appendChild(dice); */
      console.log("roll dice");
 
-       message.textContent = "dice rolled: ";
-    }
+     
     
-    console.log("convert numbers to dots");
+    
+    console.log("message rolled ok");
        
       
    
