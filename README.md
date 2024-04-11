0e7265642f647c7a09b645893fb36179a3437f17
Christina_5P Åhman <christina.ahman@tv4.se>
2024-04-11 04:45:01 +0000

    add favicon, img

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Are-you-a-dice-player
-
+https://balsamiq.com/ - Build the skeleton
 
 https://www.canva.com/
 Pictures when you win or loose
 
 https://betterprogramming.pub/creating-dice-in-flexbox-in-css-a02a5d85e516
-Tutoring to create dice with dots with css
+Tutoring/inspiration to create dice with dots with css
 
 ChatGPT- 
 To get codes explained in Swedish
+
+https://dcode.domenade.com/tutorials/how-to-create-a-dice-roll-game-with-html-css-and-javascript -tutoring dice and dots
+
+https://github.com/bradtraversy/btre_project/issues/9 - troubleshot url-problems
+
+https://favicon.io/ - For favicon to head
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,84 @@ const playElement = document.getElementById("play");
          // This is variables
       const buttons = document.getElementsByClassName("control");
       const playerScore = document.getElementById("player-score");
-      const dice = document.getElementById("dice");
-      const diceValue = document.getElementById("dice-value");
-      const choices = ["1", "2", "3", "4", "5", "6"];
-     const numberOfRolls = 6
+      //const dice = document.getElementsByclassName("dice");
+      //const dicedot = document.getElementsByclassName("dicedot");
+      const choice = ["1", "2", "3", "4", "5", "6"]; // players choice of number
+     // const numberOfRolls = 6
              
-// roll Dice and get computers number
+         function playerchoice() { 
+        choice= [i];
+        for (i=0;i<els.length;i++) {
+        if(els[i].type=='checkbox')
+        els[i].checked=!els[i].checked;
+        }
+        }
+
+     function createDice(number){
+      const dotPositionMatrix = {
+        1: [
+          [50, 50]
+        ],
+        2: [
+          [20, 20],
+          [80, 80]
+        ],
+        3: [
+          [20, 20],
+          [50, 50],
+          [80, 80]
+        ],
+        4: [
+          [20, 20],
+          [20, 80],
+          [80, 20],
+          [80, 80]
+        ],
+        5: [
+          [20, 20],
+          [20, 80],
+          [50, 50],
+          [80, 20],
+          [80, 80]
+        ],
+        6: [
+          [20, 20],
+          [20, 80],
+          [50, 20],
+          [50, 80],
+          [80, 20],
+          [80, 80]
+        ]
+      };
+    
+      const dice = document.createElement("div");
+      dice.classList.add("dice");
+
+      for (const dotPosition of dotPositionMatrix[number]) {
+        const dot = document.createElement("div");
+
+        dot.classList.add("dicedot");
+        dot.style.setProperty("--top", dotPosition[0] + "%");
+        dot.style.setProperty("--left", dotPosition[1] + "%");
+        dice.appendChild(dot);
+      }
+      return dice;
+    }
+
+
+     // roll Dice and get computers number
       function rollDice() {
-        const numberOfRolls = dice.querySelectorAll("span").length;
+         const numberOfRolls = dice.querySelectorAll("span").length;
         const randomNumber = Math.floor(Math.random() * numberOfRolls) + 1;
       console.log ("dice number");
 
-     /*change random number to dots
+     //change random number to dots
         for (let i = 0; i < randomNumber; i++) {
         const dot = document.createElement("span");
-          dot.classList.add("roll");
-        dice.appendChild(dot);//to show dot in dice through DOM         
-        }*/
+          dot.classList.add("dice");
+        dice.appendChild(dot);
+              
+        }
 
         // update dice with the number                 
       // dice.textContent = dicepatterns(randomNumber);
